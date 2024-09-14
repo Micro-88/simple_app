7160b2c096cc4e0ed2112f4669725078af520f49
Micro-88 <xXgen88Xx@gmail.com>
2024-09-15 00:34:11 +0800

    added based image of wharewolf

--- simple_app/lib/card_builder.dart
+++ simple_app/lib/card_builder.dart
@@ -10,11 +10,28 @@ Widget buildCard(String text, bool isFront) {
     child: Container(
       width: 300,
       height: 200,
-      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/card_background1.png'), // Add the image path here
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Center(
         child: Text(
           text,
-          style: TextStyle(fontSize: 24),
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white, // Adjust text color for visibility
+            fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                offset: Offset(2.0, 2.0),
+                blurRadius: 3.0,
+                color: Colors.black,
+              ),
+            ],
+          ),
           textAlign: TextAlign.center,
         ),
       ),
