2d788480d18f6e4f6962ed21b480f24e0e654082
Micro-88 <xXgen88Xx@gmail.com>
2024-09-14 23:33:32 +0800

    added back and front flip

--- simple_app/lib/main.dart
+++ simple_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math'; // Import this for pi
 
 void main() {
   runApp(MyApp());
@@ -10,36 +11,100 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Centered Card Example'),
+          title: Text('Flip Card Example'),
         ),
-        body: Center(
-          child: Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Container(
-              padding: EdgeInsets.all(20),
-              width: 300,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    'This is a centered card',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'You can add more content here.',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
+        body: FlipCard(),
+      ),
+    );
+  }
+}
+
+class FlipCard extends StatefulWidget {
+  @override
+  _FlipCardState createState() => _FlipCardState();
+}
+
+class _FlipCardState extends State<FlipCard> {
+  bool isFront = true;
+
+  void _flipCard() {
+    setState(() {
+      isFront = !isFront;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: _flipCard, // Flip the card when tapped
+        child: AnimatedSwitcher(
+          duration: Duration(milliseconds: 500),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            final rotate = Tween(begin: pi, end: 0.0).animate(animation);
+            return AnimatedBuilder(
+              animation: rotate,
+              child: child,
+              builder: (context, child) {
+                final isUnder = (ValueKey(isFront) != child?.key);
+                final value = isUnder ? min(rotate.value, pi / 2) : rotate.value;
+                return Transform(
+                  transform: Matrix4.rotationY(value),
+                  child: child,
+                  alignment: Alignment.center,
+                );
+              },
+            );
+          },
+          child: isFront
+              ? _buildFrontCard() // Front of the card
+              : _buildBackCard(), // Back of the card
+          switchInCurve: Curves.easeInOut,
+          switchOutCurve: Curves.easeInOut,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFrontCard() {
+    return Card(
+      key: ValueKey(true),
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        width: 300,
+        height: 200,
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Text(
+            'Elijah',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackCard() {
+    return Card(
+      key: ValueKey(false),
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        width: 300,
+        height: 200,
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Text(
+            'BADING',
+            style: TextStyle(fontSize: 24),
           ),
         ),
       ),
     );
   }
 }
-
